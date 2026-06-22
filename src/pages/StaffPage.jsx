7cf62e84465bdf6079c62db25b8abbe15db6c6bf
commonpsychology <commonpsychology@users.noreply.github.com>
2026-06-22 08:47:02 +0545

    add supabase

--- src/pages/StaffPage.jsx
+++ src/pages/StaffPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "YOUR_SUPABASE_URL";
 const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
